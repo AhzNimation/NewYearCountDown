2e2fc2ce44c53c12d7da48ca5f4ce6bcd09ed69d
AhzNimation <fatihahzaqusyairi24@gmail.com>
2021-12-22 17:32:05 +0800

    change JS a little bit

--- newYearScript.js
+++ newYearScript.js
@@ -6,7 +6,7 @@ const y2 = document.getElementById("year2");
 
 const SeeFireWorkBtn = document.getElementById("SNYFW");
 
-var seeNewYearFireWork = false;
+var seeNewYearFireWork = true;
 
 function CountDown() {
   var date = new Date();
@@ -17,6 +17,7 @@ function CountDown() {
   var jam = time.getHours();
   var menit = time.getMinutes();
   var detik = time.getSeconds();
+  var miliDetik = time.getMilliseconds();
 
   var tujuanTahun = tahun + 1;
   var tujuanBulan = 1;
@@ -24,6 +25,7 @@ function CountDown() {
   var tujuanJam = 0;
   var tujuanMenit = 0;
   var tujuanDetik = 0;
+  var tujuanMD = 0;
 
   if (((tujuanTahun <= tahun) && (tujuanBulan <= bulan) && (tujuanTanggal <= tanggal)) || ((bulan == 1) && (seeNewYearFireWork == true))) {
     NewYearCountDown.classList.add("hide");
@@ -34,19 +36,19 @@ function CountDown() {
   }
 
   // S... | S = selisih
-  var sTahun = tujuanTahun - tahun - 1;
+  var sTahun;
   var sBulan;
-  sBulan = TimeSet(tujuanBulan, bulan, sBulan, 12);
   var sTanggal;
   var TanggalMax = bulanf(tahun, bulan);
-  sTanggal = TimeSet(tujuanTanggal, tanggal, sTanggal, TanggalMax);
   var sJam;
-  if (tujuanJam < jam) {
-    sJam = (tujuanJam + 24 - jam - 1);
-  };
   var sMenit;
-  sMenit = TimeSet(tujuanMenit, menit, sMenit, 60);
   var sDetik;
+  sTahun = TimeSetV2(tujuanBulan, bulan, 0,sTahun, tujuanTahun, tahun);
+  sBulan = TimeSetV2(tujuanTanggal, tanggal, 12, sBulan, tujuanBulan, bulan);
+  sTanggal = TimeSetV2(tujuanJam, jam, TanggalMax, sTanggal, tujuanTanggal, tanggal);
+  sJam = TimeSetV2(tujuanMenit, menit, 24, sJam, tujuanJam, jam);
+  sMenit = TimeSet(tujuanMenit, menit, sMenit, 60);
+  sDetik;
   if (tujuanDetik <= detik) {
     sDetik = (tujuanDetik + 60 - detik - 1);
   }
@@ -84,8 +86,15 @@ function bulanf(tahun, bulan) {
   }
   return maxTanggal;
 }
+function TimeSetV2(tujuanNextVal, nextVal, maxNextVal, output, tujuanVal, defaultVar){
+  if(tujuanNextVal < nextVal){
+    output = tujuanVal + maxNextVal - defaultVar - 1;
+  } else if (tujuanNextVal >= nextVal){
+    output = tujuanVal - defaultVar;
+  } return output;
+}
 function TimeSet(tujuanVar, defaultVar, output, maxVal) {
-  if (tujuanVar < defaultVar) {
+  if (tujuanVar <= defaultVar) {
     output = (tujuanVar + maxVal - defaultVar - 1);
   } else {
     output = (tujuanVar - defaultVar);
@@ -94,6 +103,8 @@ function TimeSet(tujuanVar, defaultVar, output, maxVal) {
 function lessThan(invalue, outvalue) {
   if (invalue < 10) {
     outvalue = "0" + invalue;
+  }else if (invalue < 0) {
+    outvalue = "00";
   } else {
     outvalue = "" + invalue;
   }
@@ -112,3 +123,23 @@ function seeFireWork() {
   }
 }
 CountDown();
+
+
+
+// ! NOT USED
+  // var sTahun = tujuanTahun - tahun;
+  // var sBulan;
+  // sBulan = TimeSet(tujuanBulan, bulan, sBulan, 12);
+  // var sTanggal;
+  // var TanggalMax = bulanf(tahun, bulan);
+  // sTanggal = TimeSet(tujuanTanggal, tanggal, sTanggal, TanggalMax);
+  // var sJam;
+  // if (tujuanJam < jam) {
+  //   sJam = (tujuanJam + 24 - jam - 1);
+  // };
+  // var sMenit;
+  // sMenit = TimeSet(tujuanMenit, menit, sMenit, 60);
+  // var sDetik;
+  // if (tujuanDetik <= detik) {
+  //   sDetik = (tujuanDetik + 60 - detik - 1);
+  // }
