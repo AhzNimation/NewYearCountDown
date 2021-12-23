bf27399fd773ab07025e8c9aef4f0101c80ae0ce
AhzNimation <fatihahzaqusyairi24@gmail.com>
2021-12-23 21:02:48 +0800

    this file(fireworks.js) is not mine, source code link : https://codepen.io/whqet/pen/Auzch

--- fireworks.js
+++ fireworks.js
@@ -1,7 +1,6 @@
 /* SOURCE CODE
 https://codepen.io/whqet/pen/Auzch */
 
-
 // when animating on canvas, it is best to use requestAnimationFrame instead of setTimeout or setInterval
 // not supported in all browsers though and sometimes needs a prefix, so we need a shim
 window.requestAnimFrame = ( function() {
